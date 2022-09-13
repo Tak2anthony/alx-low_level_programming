7b1eeab292a19d8040e36345d7414a8663f5d314
Thomas Idowu Lawrence <tak2anthony@gmail.com>
2022-09-13 13:12:21 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,46 @@
-#include "main.h"
-/**
- * times_table -prints tables
- *
- * Return: Always 0.
- */
-void times_table(void)
-{
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
-_putchar ('\n');
-}
-}
+#include "main.h" 
+  
+ /** 
+  * times_table - function that prints times table for 9 
+  * 
+ */ 
+  
+ void times_table(void) 
+ { 
+         int x = 0; /* factor */ 
+         int y; /* count  */ 
+         int z; /* computed value */ 
+  
+         while (x < 10) 
+         { 
+                 y = 0; 
+                 while (y < 10) 
+                 { 
+                         z = x * y; 
+  
+                         if (z > 9) 
+                         { 
+                                 _putchar(z / 10 + '0'); 
+                                 _putchar(z % 10 + '0'); 
+                         } 
+                         else if (y != 0) 
+                         { 
+                                 _putchar(' '); 
+                                 _putchar(z + '0'); 
+                         } 
+                         else 
+                         { 
+                                 _putchar(z + '0'); 
+                         } 
+  
+                         if (y != 9) 
+                         { 
+                                 _putchar(','); 
+                                 _putchar(' '); 
+                         } 
+                         y++; 
+                 } 
+                 _putchar('\n'); 
+                 x++; 
+         } 
+ }
