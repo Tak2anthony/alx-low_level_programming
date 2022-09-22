a1678cca3bcf8d330ba27cb83fdab9765754f6cd
Thomas Idowu Lawrence <tak2anthony@gmail.com>
2022-09-22 06:05:31 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-                                                      /**
- * print_number - A function to print inputed
- * number as it is
- * @n:number to be printed.
- */                                                   
-void print_number(int n)
+
+/**
+ * print_number - function to print int using _putchar
+ *
+ * @num: int to print
+ */
+void print_number(int num)
 {
-        unsigned int n1;
-                                                              if (n < 0)
-        {
-                n1 = -n;                                              putchar('-');
-        }
-        else
-        {
-                n1 = n;                                       }
-                                                              if (n1 / 10)
-                print_number(n1 / 10);                        putchar((n1 % 10) + '0');
+	unsigned int d = 10, n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num *= -1;
+	}
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
