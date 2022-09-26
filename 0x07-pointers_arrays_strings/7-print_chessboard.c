b25d92951da4427d05570009106f32e0af709d2a
Thomas Idowu Lawrence <tak2anthony@gmail.com>
2022-09-26 06:01:22 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_chessboard - prints the chessboard
+ *@a:array input
+ *
+ *Return:void
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+		_putchar('\n');
+	}
