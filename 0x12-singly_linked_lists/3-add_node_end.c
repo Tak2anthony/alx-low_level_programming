d99b4bcb124bd2a0a5f8c9975e39163897c9ecf4
Thomas Idowu Lawrence <tak2anthony@gmail.com>
2022-10-20 18:18:40 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,46 @@
-nclude <string.h>
-#include <stdlib.h>
-#include "lists.h"
+nclude "lists.h"
 
 /**
- *   * *add_node_end - adds a new node at the end of a linked list.
- *     * @head: start of the list.
- *       * @str: string.
- *         * Return: the address of the new element,
- *           * or null if it failed.
- *             */
+ *  * add_node_end - Adds a new node at the end of a list
+ *   *@head: pointer to the head of the list
+ *    *@str: string to be added to the list
+ *     *
+ *      *Return: NULL in case of failure
+ *       *or address or the element
+ *       */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-		list_t *newNode;
+		char *dup;
+			int len;
+				list_t *new, *last;
 
-			newNode = (list_t *)malloc(sizeof(list_t));
-				if (newNode == NULL)
-							return (NULL);
-					newNode->str = strdup(str);
-						newNode->len = string_length(str);
-							newNode->next = NULL;
-								/* check if list is null and make new node the head */
-								if (*head == NULL)
-											*head = newNode;
-									/* else find last node and add new node */
-									else
-											{
-														list_t *lastNode = *head;
-																/* last node has to be NULL use while to check */
-																while (lastNode->next != NULL)
-																			{
-																							lastNode = lastNode->next;
-																									}
-																		/* if null is found add new node at the end */
-																		lastNode->next = newNode;
-																			}
-										return (newNode);
-}
+					new = malloc(sizeof(list_t));
+						if (new == NULL)
+									return (NULL);
 
-/**
- *   * string_length - finds the length of a string.
- *     * Return: length of c.
- *       * @pointer: pointer.
- *         */
-int string_length(const char *pointer)
-{
-		int c = 0;
+							dup = strdup(str);
+								if (str == NULL)
+										{
+													free(new);
+															return (NULL);
+																}
 
-			while (*(pointer + c) != '\0')
-					{
-								c++;
-									}
-				return (c);
+									for (len = 0; str[len];)
+												len++;
+
+										new->str = dup;
+											new->len = len;
+												new->next = NULL;
+
+													if (*head == NULL)
+																*head = new;
+														else
+																{
+																			last = *head;
+																					while (last->next != NULL)
+																									last = last->next;
+																							last->next = new;
+																								}
+															return (*head);
 }
